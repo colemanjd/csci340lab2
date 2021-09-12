a703a4140571be9ef4eecf0b4e42b602c38e055b
colemanjd <89943052+colemanjd@users.noreply.github.com>
2021-09-11 21:48:19 -0500

    Update script.js
    Random Grab Attempt

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+$(document).ready(function() {
+  $('.animebutton').click(function() {
+    $.ajax({
+      dataType: "jsonp",
+      jsonpCallback: "parseQuote",
+      url: "https://animechan.vercel.app/api/random",
+      success: function(results) {
+        $('.animepick').text(results["quoteText"]);
+      },
+      error: function(xhr,status,error) {
+        console.log(error);
+      }
+    });
+  });
+});
 
+
+$(document).ready(function() {
+  $('.activitybutton').click(function() {
+    $.ajax({
+      dataType: "jsonp",
+      jsonpCallback: "parseQuote",
+      url: "http://www.boredapi.com/api/activity/",
+      success: function(results) {
+        $('.activitypick').text(results["quoteText"]);
+      },
+      error: function(xhr,status,error) {
+        console.log(error);
+      }
+    });
+  });
+});
