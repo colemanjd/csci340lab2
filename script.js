47156697ea25c34545ca7a9b152ba0e7e72822c7
colemanjd <89943052+colemanjd@users.noreply.github.com>
2021-09-11 21:57:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 $(document).ready(function() {
   $('.animebutton').click(function() {
     $.ajax({
       dataType: "jsonp",
       jsonpCallback: "parseQuote",
-      url: "https://animechan.vercel.app/api/random",
+      url: "https://animechan.vercel.app/guide#random-quote",
       success: function(results) {
         $('.animepick').text(results["quoteText"]);
       },
@@ -30,3 +31,28 @@ $(document).ready(function() {
     });
   });
 });
+
+ <div id="counts"></div>
+  <div id="results"></div>
+  <script>
+    var numyes = 0;
+     $("#counts").text("You clicked " + numyes + " times");
+    $("#yesbutton").on("click", function(event){
+        numyes++;
+     $("#results").text("The Anime Quote and Activity have Correlated " + numyes + " times");
+    
+     });
+    
+    var numno = 0;
+     $("#counts").text("You clicked " + numno + " times");
+    $("#nobutton").on("click", function(event){
+        numyes++;
+    $("#results").text("The Anime Quote and Activity haven't Correlated " + numyes + " times");
+     });
+    
+  
+     $("#results").text("Your page has a " + (numyes/(numyes+numno)) + "% Correlation Percentage");
+    
+ 
+    
+    </script>
